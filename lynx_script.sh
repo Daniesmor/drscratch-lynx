fa15066efc0bb87e9d9ff7b2b533d7feee0581d5
Daniel Escobar <daniesmor@gmail.com>
2024-07-25 13:26:21 +0200

    Create lynx_script.sh

---
+++ lynx_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please introduce the path of the .SB3 file"
+read sb_path
+
+curl -c cookies.txt https://www.drscratch.org/
+
+csrf_token=$(grep csrftoken cookies.txt | cut -f 7)
+
+curl -s -b cookies.txt -F "csrfmiddlewaretoken=$csrf_token" -F "dashboard_mode=Default" -F "urlProject=" -F "zipFile=@/home/daniel/Downloads/Albedo Project.sb3" -F "_upload=['']" https://www.drscratch.org/show_dashboard/ > temp.html
+
+lynx temp.html
