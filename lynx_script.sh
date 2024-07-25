eb5f20871862ea3b3ccf731d480689fcbbe0adf8
Daniel Escobar <daniesmor@gmail.com>
2024-07-25 13:56:29 +0200

    Update lynx_script.sh

--- lynx_script.sh
+++ lynx_script.sh
@@ -7,6 +7,6 @@ curl -c cookies.txt https://www.drscratch.org/
 
 csrf_token=$(grep csrftoken cookies.txt | cut -f 7)
 
-curl -s -b cookies.txt -F "csrfmiddlewaretoken=$csrf_token" -F "dashboard_mode=Default" -F "urlProject=" -F "zipFile=@/home/daniel/Downloads/Albedo Project.sb3" -F "_upload=['']" https://www.drscratch.org/show_dashboard/ > temp.html
+curl -s -b cookies.txt -F "csrfmiddlewaretoken=$csrf_token" -F "dashboard_mode=Default" -F "urlProject=" -F "zipFile=@/home/daniel/Downloads/$sb_path" -F "_upload=['']" https://www.drscratch.org/show_dashboard/ > temp.html
 
 lynx temp.html
